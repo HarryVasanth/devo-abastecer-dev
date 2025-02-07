f33148a8dd7dc978eb18253fc31c4c3cd7bd34e1
Harry <mailsuthan@gmail.com>
2025-02-07 18:42:30 +0000

    fix(post_facebook): logic

--- post_facebook.py
+++ post_facebook.py
@@ -1,12 +1,33 @@
 import os
 import requests
 
+from constants import (
+    CURRENT_WEEK,
+    START_DATE_KEY,
+    DIESEL_TW,
+    GASOLINE_95_TW,
+    GASOLINE_98_TW,
+    END_DATE_KEY,
+    PREVIOUS_WEEK,
+    DIESEL,
+    GAS_KEY,
+    GASOLINE_95,
+    GASOLINE_98,
+)
+
 # Replace with your Page ID and Access Token
 PAGE_ID = os.environ["FACEBOOK_PAGE_ID"]
 ACCESS_TOKEN = os.environ["FACEBOOK_ACCESS_TOKEN"]
 
 GRAPH_API_URL = "https://graph.facebook.com/v22.0"
 
+def gas_prices_message(price_current, price_previous):
+    if price_current > price_previous:
+        return f"{price_current}€   ⬆️   {price_previous}€"
+    if price_current < price_previous:
+        return f"{price_current}€   ⬇️️   {price_previous}€"
+    if price_current == price_previous:
+        return f"{price_current}€   =   {price_previous}€"
 
 def post_text_to_facebook(message):
     """
@@ -56,24 +77,14 @@ def make_facebook_post(dict_prices):
     """
     Format and post a message to the Facebook page.
     """
-    # Format the message
+    # Format post
     post_message = "— Devo abastecer? ⛽️ \n\n"
-    post_message += f"         {dict_prices['CURRENT_WEEK']['START_DATE_KEY']}  |  {dict_prices['PREVIOUS_WEEK']['START_DATE_KEY']}\n"
+    post_message += f"         {dict_prices[CURRENT_WEEK][START_DATE_KEY]}  |  {dict_prices[PREVIOUS_WEEK][START_DATE_KEY]}\n"
     post_message += "                      a         |            a\n"
-    post_message += f"         {dict_prices['CURRENT_WEEK']['END_DATE_KEY']}  |  {dict_prices['PREVIOUS_WEEK']['END_DATE_KEY']}\n\n"
-
-    def gas_prices_message(price_current, price_previous):
-        if price_current > price_previous:
-            return f"{price_current}€   ⬆️   {price_previous}€"
-        elif price_current < price_previous:
-            return f"{price_current}€   ⬇️️   {price_previous}€"
-        else:
-            return f"{price_current}€   =   {price_previous}€"
-
-    # Add prices
-    post_message += f"DIESEL: {gas_prices_message(dict_prices['CURRENT_WEEK']['GAS_KEY']['DIESEL'], dict_prices['PREVIOUS_WEEK']['GAS_KEY']['DIESEL'])}\n"
-    post_message += f"GASOLINE 95: {gas_prices_message(dict_prices['CURRENT_WEEK']['GAS_KEY']['GASOLINE_95'], dict_prices['PREVIOUS_WEEK']['GAS_KEY']['GASOLINE_95'])}\n"
-    post_message += f"GASOLINE 98: {gas_prices_message(dict_prices['CURRENT_WEEK']['GAS_KEY']['GASOLINE_98'], dict_prices['PREVIOUS_WEEK']['GAS_KEY']['GASOLINE_98'])}\n"
+    post_message += f"         {dict_prices[CURRENT_WEEK][END_DATE_KEY]}  |  {dict_prices[PREVIOUS_WEEK][END_DATE_KEY]}\n\n"
+    post_message += f"{DIESEL_TW}{gas_prices_message(dict_prices[CURRENT_WEEK][GAS_KEY][DIESEL], dict_prices[PREVIOUS_WEEK][GAS_KEY][DIESEL])}\n"
+    post_message += f"{GASOLINE_95_TW}{gas_prices_message(dict_prices[CURRENT_WEEK][GAS_KEY][GASOLINE_95], dict_prices[PREVIOUS_WEEK][GAS_KEY][GASOLINE_95])}\n"
+    post_message += f"{GASOLINE_98_TW}{gas_prices_message(dict_prices[CURRENT_WEEK][GAS_KEY][GASOLINE_98], dict_prices[PREVIOUS_WEEK][GAS_KEY][GASOLINE_98])}\n"
 
     # Post the message
     return post_text_to_facebook(post_message)
